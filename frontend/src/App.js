c92f6e3455854a7c39fb093e026b19e8afb32416
benswaving <benswaving@gmail.com>
2024-10-25 07:29:23 +0000

    Add basic frontend React setup and backend Express setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dropship Website</h1>
+        <p>Welcome to our dropshipping platform</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
